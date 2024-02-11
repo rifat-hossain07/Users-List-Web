0aa730d2de3e2f1549ac3e2a2e97a9b01fb8e65d
Rifat Risam <rifatrabby78@gmail.com>
2024-02-11 19:11:53 +0600

    Footer added and design updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Footer from "./Components/Footer";
 function App() {
   return (
     <div className="text-black">
       <Navbar />
       <Outlet />
+      <Footer />
       <ToastContainer />
     </div>
   );
